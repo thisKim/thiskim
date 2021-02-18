adae292a0ceb20dea4daea8dd552ac158109a617
thisKim <kimba161@gmail.com>
2021-02-18 15:49:18 -0800

    move ol

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>thiskim.com</h1>
-        <ol>
-          <li>
-            npx create-react-app helloWorld
-          </li>
-          <li>
-            create helloWorld repo on GitHub
-          </li>
-          <li>
-            connect GitHub and AWS Amplify
-          </li>
-          <li>
-            configure DNS
-          </li>
-          <li>
-            ???
-          </li>
-          <li>
-            Profit
-          </li>
-        </ol>
       </header>
+      <ol>
+        <li>
+          npx create-react-app helloWorld
+        </li>
+        <li>
+          create helloWorld repo on GitHub
+        </li>
+        <li>
+          connect GitHub and AWS Amplify
+        </li>
+        <li>
+          configure DNS
+        </li>
+        <li>
+          ???
+        </li>
+        <li>
+          Profit
+        </li>
+      </ol>
     </div>
   );
 }
